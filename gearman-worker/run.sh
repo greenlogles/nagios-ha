29cbb88268b79c3f1cbd119328116ddd870851b9
Alexandr Shut <alexandr.shut@gmail.com>
2017-09-18 08:40:40 +0300

    fix startup scripts

--- gearman-worker/run.sh
+++ gearman-worker/run.sh
@@ -5,16 +5,16 @@ GEARMAN_WORKER_CONF=/opt/nagios/etc/mod_gearman/worker.conf
 
 CMD="/opt/nagios/bin/mod_gearman_worker --config=$GEARMAN_WORKER_CONF --logmode=stdout --debug=1"
 
-if [ -z $GEARMAN_SERVERS ]; then
+if [ ! -z "$GEARMAN_SERVERS" ]; then
 	CMD="$CMD --server=$GEARMAN_SERVERS"
 fi
 
-#if [ -z $GEARMAN_SEPARATED_HOSTGROUPS ]; then
+#if [ ! -z "$GEARMAN_SEPARATED_HOSTGROUPS" ]; then
 #	echo "hostgroups=$GEARMAN_SEPARATED_HOSTGROUPS" >> $MOD_GEARMAN_CONF
 #fi
 
-if [ -z $GEARMAN_KEY ]; then
+if [ ! -z "$GEARMAN_KEY" ]; then
 	CMD="$CMD --key=$GEARMAN_KEY"
 fi
-
+echo $CMD
 sudo -u $NAGIOS_USER $CMD
--- nagios-server/run.sh
+++ nagios-server/run.sh
@@ -3,15 +3,15 @@
 NAGIOS_USER=nagios
 MOD_GEARMAN_CONF=/opt/nagios/etc/mod_gearman/module.conf
 
-if [ -z $GEARMAN_SERVERS ]; then
+if [ ! -z "$GEARMAN_SERVERS" ]; then
 	sed -i "s|server=127.0.0.1:4730|server=$GEARMAN_SERVERS|g" $MOD_GEARMAN_CONF
 fi
 
-if [ -z $GEARMAN_SEPARATED_HOSTGROUPS ]; then
+if [ ! -z "$GEARMAN_SEPARATED_HOSTGROUPS" ]; then
 	echo "hostgroups=$GEARMAN_SEPARATED_HOSTGROUPS" >> $MOD_GEARMAN_CONF
 fi
 
-if [ -z $GEARMAN_KEY ]; then
+if [ ! -z "$GEARMAN_KEY" ]; then
 	sed -i "s|key=should_be_changed|key=$GEARMAN_KEY|g" $MOD_GEARMAN_CONF
 fi
 
